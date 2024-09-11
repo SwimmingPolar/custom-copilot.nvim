9e0c04121d4b886a16f1605f1faba8e1be293592
YDH <swimming.polar@gmail.com>
2024-09-11 21:35:11 +0900

    modified test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,4 @@ find . -type f | entr -c nvim --headless \
     -c "set rtp+=$top_dir" \
     -c "runtime $PLUGIN_NAME/**/*.{vim,lua}" \
     -c "runtime $PLUGIN_NAME/tests/minimal_init.lua" \
-    -c "PlenaryBustedDirectory tests"
+    -c "PlenaryBustedDirectory tests { 'minimal_init' }"
