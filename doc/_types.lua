0c773ba98f6e0e01e73e66e48afc273466cfab37
YDH <swimming.polar@gmail.com>
2024-09-13 16:06:26 +0900

    updated types

--- doc/_types.lua
+++ doc/_types.lua
@@ -10,12 +10,11 @@
 ---@field data any Arbitrary data passed from nvim_exec_autocmds().
 
 ---@class on_autocmd_opts
----@field autocmds table<string>
+---@field callback fun(event_args: autocmd_event): boolean|string? Lua function or Vimscript function name. Returns truthy value to delete the autocommand.
+---@field pattern string|string[] Pattern(s) to match.
 ---@field group string|integer? Autocommand group name or id.
----@field pattern string|string[]? Pattern(s) to match.
 ---@field buffer integer? Buffer number for buffer-local autocommands.
 ---@field desc string? Description for documentation and troubleshooting.
----@field callback fun(event_args: autocmd_event): boolean|string? Lua function or Vimscript function name. Returns truthy value to delete the autocommand.
 ---@field command string? Vim command to execute on event.
 ---@field once boolean? Defaults to false. Run the autocommand only once.
 ---@field nested boolean? Defaults to false. Run nested autocommands.
