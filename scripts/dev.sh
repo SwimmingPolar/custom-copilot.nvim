f8fea1b7ca720f7f4c24680359dec58b90d8f8e3
YDH <swimming.polar@gmail.com>
2024-09-21 00:05:35 +0900

    added: manifest env var

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,9 @@
 top_dir=$1
 PLUGIN_NAME=$2
 
+# set a path for manifest file
+export NVIM_RPLUGIN_MANIFEST="$top_dir/rplugin.vim"
+
 # add the current dir to runtimepath  \
 # change rplugin.vim manifest path to ./rplugin.vim \
 # load the current plugin dir
@@ -8,5 +11,4 @@ nvim \
     -c "set rtp+=$top_dir" \
     -c "let g:is_dev = 1" \
     -c "let g:python3_host_prog = 'venv/bin/python3'" \
-    -c "let \$NVIM_RPLUGIN_MANIFEST = '$top_dir/rplugin.vim'" \
     -c "\"runtime $PLUGIN_NAME/**/*.{vim,lua}\"" # add the current dir to runtimepath \
