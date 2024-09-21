0671a9dabfaa72203d50dba233685e6313c9157e
YDH <swimming.polar@gmail.com>
2024-09-21 13:08:57 +0900

    loading plugin explicitly on startup, not lazily

--- plugin/custom-copilot.lua
+++ plugin/custom-copilot.lua
@@ -7,3 +7,15 @@ if vim.g.loaded_custom_copilot == 1 then
     return
 end
 vim.g.loaded_custom_copilot = 1
+
+local plugin_ok, plugin_load_err = pcall(function()
+    require("custom-copilot").setup()
+end)
+
+-- Check if running on dev mode
+if vim.g.is_dev == 1 then
+    -- error while loading lua plugin
+    if not plugin_ok then
+        vim.print(plugin_load_err)
+    end
+end
